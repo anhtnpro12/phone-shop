69eacc3a602814da775684b36efcaa47f3d122d5
AnhTN <anhyc12@gmail.com>
2023-04-20 17:02:07 +0700

    update

--- day5/Tong Hop with more connection/script.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE user;
-
-CREATE TABLE `users`. (`id` INT NOT NULL AUTO_INCREMENT , `user_name` VARCHAR(100) NOT NULL 
-, `password` VARCHAR(100) NOT NULL , `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NULL 
-, `age` INT NULL , `address` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci NULL , `sex` BOOLEAN NULL 
-, `last_active` DATETIME NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
--- day5/Tong Hop with more connection/views/logout.php
+++
@@ -1,9 +0,0 @@
-<?php    
-    require('../dal/UserDAO.php');
-    session_start();
-
-    UserDAO::logout($_SESSION['user']->getId());    
-    
-    unset($_SESSION['user']);    
-    header('Location:./login.php');
-?>
