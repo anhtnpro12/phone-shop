a22030de7e4b46d90ac526dbe996aae8686e904b
Trinh Nam Anh <107114928+anhtnpro12@users.noreply.github.com>
2023-04-25 13:29:48 +0700

    update learning information day7

---
+++ day7/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$helloWorld = function() {
+    echo "asdf";
+};
+
+function show($mess) {
+
+}
+
+// $message = create_function('', 'echo "Hello world!";');
+
+$user = "AnhTN";
+
+$hello = function() use ($user) {
+    echo "Hello $user";
+    $user = 'hahahah';
+};
+
+$hello();
+echo $user;
+
