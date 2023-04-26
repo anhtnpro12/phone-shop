f4f6c3184fded48b55e6076ac03727c8de6eaa81
AnhTN <anhyc12@gmail.com>
2023-04-26 10:01:55 +0700

    update code day 6 lan 5

--- day6/Exercise/TongHop/views/orders/create.php
+++ day6/Exercise/TongHop/views/orders/create.php
@@ -1,8 +1,16 @@
 <?php
 
+use DataAccessLayer\CustomerDAO;
+use DataAccessLayer\PaymentDAO;
+use DataAccessLayer\ProductDAO;
+use DataAccessLayer\ShippingDAO;
+
 $page = 'order';
 require '../components/header.php'; 
 include '../../dal/CustomerDAO.php';
+include '../../dal/ShippingDAO.php';
+include '../../dal/PaymentDAO.php';
+include '../../dal/ProductDAO.php';
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
@@ -12,29 +20,72 @@ if (isset($_POST['submit'])) {
     
 }
 
+$products = ProductDAO::getList($conn);
+$customers = CustomerDAO::getList($conn);
+$shippings = ShippingDAO::getList($conn);
+$payments = PaymentDAO::getList($conn);
 
 
 ?>
 
 <div class="container mt-5 mb-5 d-flex justify-content-center">    
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" style="width: 50%;">
-        <h3 class="text-center">Add Shipping Method</h3>    
-
+        <h3 class="text-center">Add Order</h3>    
+        
         <div class="container mb-3 d-flex">
             <hr>
             <div class="wrapper flex-fill">
                 <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-                </div>
+                    <label for="product_id" class="form-label">Product Name</label>
+                    <select id="product_id" name="product_id" class="selectpicker" 
+                            data-live-search="true" data-width="100%" 
+                            data-style="border" data-size="5">
+                            <?php
+                
+                            foreach ($products as $key => $value) {
+                                echo "<option value='$value->id' 
+                                        data-content='<div>
+                                                        <h6>$value->name</h6>
+                                                        <small>$ $value->price</small>
+                                                    </div>' >$value->name</option>";
+                            }                    
+    
+                            ?>                                                                                                                                        
+                    </select>
+                </div>                                 
+                <div class="mb-3">
+                    <label for="quantity1" class="form-label">Quantity</label>
+                    <input type="text" name="quantity[]" class="form-control" id="quantity1" required>
+                </div>                 
             </div>
             <div class="flex-fill d-flex justify-content-end align-items-center">
-                <button class="btn btn-danger">Remove</button>
+                <button class="btn btn-danger" onclick="this.parentNode.parentNode.remove();">Remove</button>
             </div>
+        </div>
+        <div class="container mb-3 d-flex">
+            <button class="btn btn-danger" onclick=";">Add Product</button>
         </div>
         <hr>
         <div class="mb-3">
-            <label for="name" class="form-label">Creator</label>
+            <label for="amount" class="form-label">Amount</label>
+            <input type="text" name="amount" class="form-control" id="amount" required>
+        </div> 
+        <div class="mb-3">
+            <label for="customer_id" class="form-label">Creator</label>
+            <select id="customer_id" name="customer_id" class="selectpicker" 
+                    data-live-search="true" data-width="100%" 
+                    data-style="border" data-size="5">
+                <?php
+                
+                foreach ($customers as $key => $value) {
+                    echo "<option value='$value->id' >$value->name</option>";
+                }                    
+
+                ?>                                                                                                                                                      
+            </select>
+        </div>    
+        <div class="mb-3">
+            <label for="name" class="form-label">State</label>
             <select id="state" name="state" class="selectpicker" 
                     data-live-search="true" data-width="100%" 
                     data-style="border" data-size="5">
@@ -43,23 +94,51 @@ if (isset($_POST['submit'])) {
                 <option value="3" data-content='<span class="badge bg-warning">Delivery</span>'>Delivery</option>                                                                
                 <option value="4" data-content='<span class="badge bg-success">Complete</span>'>Complete</option>                                                                                                                                       
             </select>
-
         </div>    
-        <span class="badge badge-secondary">Unconfirmed</span>            
         <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-        </div>              
+            <label for="shipping_id" class="form-label">Shipping Method</label>
+            <select id="shipping_id" name="shipping_id" class="selectpicker" 
+                    data-live-search="true" data-width="100%" 
+                    data-style="border" data-size="5">
+                <?php
+            
+                foreach ($shippings as $key => $value) {
+                    echo "<option value='$value->id' >$value->name</option>";
+                }                    
+
+                ?>                                                                                                                                    
+            </select>
+        </div>    
         <div class="mb-3">
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="status" id="active" value="1" checked>
-                <label class="form-check-label" for="active">Active</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="status" id="inactive" value="0">
-                <label class="form-check-label" for="inactive">Inactive</label>
-            </div>
-        </div>        
+            <label for="shipping_id" class="form-label">Shipping Method</label>
+            <select id="shipping_id" name="shipping_id" class="selectpicker" 
+                    data-live-search="true" data-width="100%" 
+                    data-style="border" data-size="5">
+                <?php
+            
+                foreach ($shippings as $key => $value) {
+                    echo "<option value='$value->id' >$value->name</option>";
+                }                    
+
+                ?>                                                                                                                                    
+            </select>
+        </div>    
+        <div class="mb-3">
+            <label for="payment_id" class="form-label">Payment Method</label>
+            <select id="payment_id" name="payment_id" class="selectpicker" 
+                    data-live-search="true" data-width="100%" 
+                    data-style="border" data-size="5">
+                <?php
+            
+                foreach ($payments as $key => $value) {
+                    echo "<option value='$value->id' >$value->name</option>";
+                }                    
+
+                ?>                                                                                                                                    
+            </select>
+        </div>    
+              
+                                     
         
         <input type="submit" name="submit" value="Add now" class="btn btn-primary">
     </form>
