89f2844a28aa689a68cb9542c539413a38663b29
Anh Trịnh Nam <107114928+anhtnpro12@users.noreply.github.com>
2023-05-10 14:29:58 +0700

    Update create.blade.php

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.default')
 
 @section('contents')
-    <div class="container mt-5 mb-5 d-flex justify-content-center">    
+    <div class="container mt-5 mb-5 d-flex justify-content-center">
         <form action="#" method="post" style="width: 50%;">
-            <h3 class="text-center">Add Customer</h3>        
+            <h3 class="text-center">Add Customer</h3>
 
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
                 <input type="text" value="" name="name" class="form-control" id="name" required>
-            </div>                
+            </div>
             <div class="mb-3">
                 <label for="address" class="form-label">Address</label>
                 <input type="text" value="" min='0' name="address" class="form-control" id="address" required>
@@ -17,13 +17,13 @@
                 <label for="phone" class="form-label">Phone</label>
                 <input type="text" value="" name="phone" class="form-control " id="phone" required>
                 <small class="text-danger d-none">Phone already exists</small>
-            </div>        
+            </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" value="" name="email" class="form-control " id="email" required>
                 <small class="text-danger d-none">Email already exists</small>
-            </div>                
+            </div>
             <input type="submit" name="submit" value="Add now" class="btn btn-primary">
         </form>
-    </div>    
+    </div>
 @endsection
